752b9bb1bf77c4af7ea37b4e8746892fb6d6f8fe
Oleh Kovalenko <okolegko@gmail.com>
2025-07-23 22:11:52 +0300

    Changes Right Names

--- hw-20 - JavaScript Basics/assets/js/speed.js
+++
@@ -1,4 +0,0 @@
-let distance = prompt("Enter the distance between cities (km):");
-let time = prompt("How many hours do you want to arrive?");
-let speed = distance / time;
-alert("Required speed: " + speed + " km/h");
